919f7d18d696e385191b8642b3b59a13f17ed0d5
ironfox-dev <cooldev0127@gmail.com>
2021-11-22 15:38:45 +0800

    no message

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,28 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        "logo": "593px",
+      },
+      backgroundColor: {
+        "theme": "#91B539"
+      },
+      textColor: {
+        "theme": "#91B539"
+      },
+      backgroundImage: {
+        'main': "url('assets/bg.png')",
+        'header': "url('assets/bg-header.png')"
+      },
+      borderColor: {
+        "theme": "#91B539"
+      }
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
